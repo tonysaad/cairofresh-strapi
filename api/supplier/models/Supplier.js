c6909f5019d723f9e0b57176c774a32476fd0bb8
Tony Saad <tony@wego.com>
2019-11-08 01:24:34 +0100

    Calling Netlify Build Web Hook

--- api/supplier/models/Supplier.js
+++ api/supplier/models/Supplier.js
@@ -1,55 +1,32 @@
 'use strict';
+const axios = require('axios');
 
 /**
  * Lifecycle callbacks for the `Supplier` model.
  */
 
 module.exports = {
-  // Before saving a value.
-  // Fired before an `insert` or `update` query.
-  // beforeSave: async (model, attrs, options) => {},
+  afterCreate: async (entry) => {
+    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
+      .catch(() => {
+          console.error('Something went wrong while calling Netlify Web Hook');
+        }
+      );
+  },
 
-  // After saving a value.
-  // Fired after an `insert` or `update` query.
-  // afterSave: async (model, response, options) => {},
+  afterUpdate: async (entry) => {
+    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
+      .catch(() => {
+          console.error('Something went wrong while calling Netlify Web Hook');
+        }
+      );
+  },
 
-  // Before fetching a value.
-  // Fired before a `fetch` operation.
-  // beforeFetch: async (model, columns, options) => {},
-
-  // After fetching a value.
-  // Fired after a `fetch` operation.
-  // afterFetch: async (model, response, options) => {},
-  
-  // Before fetching all values.
-  // Fired before a `fetchAll` operation.
-  // beforeFetchAll: async (model, columns, options) => {},
-
-  // After fetching all values.
-  // Fired after a `fetchAll` operation.
-  // afterFetchAll: async (model, response, options) => {},
-
-  // Before creating a value.
-  // Fired before an `insert` query.
-  // beforeCreate: async (model, attrs, options) => {},
-
-  // After creating a value.
-  // Fired after an `insert` query.
-  // afterCreate: async (model, attrs, options) => {},
-
-  // Before updating a value.
-  // Fired before an `update` query.
-  // beforeUpdate: async (model, attrs, options) => {},
-
-  // After updating a value.
-  // Fired after an `update` query.
-  // afterUpdate: async (model, attrs, options) => {},
-
-  // Before destroying a value.
-  // Fired before a `delete` query.
-  // beforeDestroy: async (model, attrs, options) => {},
-
-  // After destroying a value.
-  // Fired after a `delete` query.
-  // afterDestroy: async (model, attrs, options) => {}
+  afterDestroy: async (entry) => {
+    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
+      .catch(() => {
+          console.error('Something went wrong while calling Netlify Web Hook');
+        }
+      );
+  }
 };
