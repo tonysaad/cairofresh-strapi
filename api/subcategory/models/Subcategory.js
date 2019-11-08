23797ab2c9783d73c4655c13e58936332066a7cf
Tony Saad <tony@wego.com>
2019-11-08 01:37:26 +0100

    Update netlify config var

--- api/subcategory/models/Subcategory.js
+++ api/subcategory/models/Subcategory.js
@@ -7,25 +7,28 @@ const axios = require('axios');
 
 module.exports = {
   afterCreate: async (entry) => {
-    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
-      .catch(() => {
+    axios.post(strapi.config.currentEnvironment.netlifyBuild, entry)
+      .catch((err) => {
           console.error('Something went wrong while calling Netlify Web Hook');
+          console.error(err);
         }
       );
   },
 
   afterUpdate: async (entry) => {
-    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
-      .catch(() => {
+    axios.post(strapi.config.currentEnvironment.netlifyBuild, entry)
+      .catch((err) => {
           console.error('Something went wrong while calling Netlify Web Hook');
+          console.error(err);
         }
       );
   },
 
   afterDestroy: async (entry) => {
-    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
-      .catch(() => {
+    axios.post(strapi.config.currentEnvironment.netlifyBuild, entry)
+      .catch((err) => {
           console.error('Something went wrong while calling Netlify Web Hook');
+          console.error(err);
         }
       );
   }
